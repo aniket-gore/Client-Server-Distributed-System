4f121425794e640a68d3443fe3423f2a4e3d8fb6
aniket-gore <aniketgore05@gmail.com>
2015-02-24 22:24:13 -0500

    Read config files. Added shutdown. Added persistent storage by writing to file.

--- src/client_code.go
+++ src/client_code.go
@@ -1,31 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/rpc/jsonrpc"
 	"os"
 )
 
-func main() {
-	/* Input arg
-	{"jsonrpc":"2.0","method":"lookup","params":["keyA","relationA"],"id":10}
-	*/
-	args_bytes := []byte(os.Args[1])
+type config_type map[string]interface{}
 
-	service := "127.0.0.1:8010"
+func main() {
+	// Read server configuration file
+	var config config_type
+	file, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal("Cannot open config file: ", err)
+	}
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		log.Fatal("Cannot read config file:", err)
+	}
+
+	// Set server connection parameters
+	service := config["ipAddress"].(string) + ":" + config["port"].(string)
+
+	// Set-up client-server connection
 	client, err := jsonrpc.Dial("tcp", service)
 	if err != nil {
 		log.Fatal("Dialing Error: ", err)
 	}
 
+	// Make server request
 	var response []byte
+	args_bytes := []byte(os.Args[2])
 	err = client.Call("DICT3.ServiceRequest", args_bytes, &response)
-	fmt.Println("Response received!!!")
 	if err != nil {
 		log.Fatal("Lookup error", err)
 	}
-	fmt.Println("Response: ", string(response))
-	//	write_to_console := os.Stdout
-	//	fmt.Fprintf(write_to_console, "Response to console")
+	write_to_console := os.Stdout
+	fmt.Fprintf(write_to_console, string(response))
 }
--- src/server_code.go
+++ src/server_code.go
@@ -3,66 +3,188 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net"
 	"net/rpc"
 	"net/rpc/jsonrpc"
 	"os"
 )
 
+// structure for response object
 type Response struct {
 	Fields map[string]interface{}
 }
 
+// structure for request object
 type Request struct {
 	Fields map[string]interface{}
 }
 
+// structure for persistence storage
 type DICT3 struct {
-	key, relation, value string
+	Triplet  map[string]map[string]interface{}
+	listener net.Listener
 }
 
-func lookup(request Request) Response {
-	fmt.Println("Unmarshalled request: ", request)
-	return Response{}
-}
+type config_type map[string]interface{}
 
-func (dict3 *DICT3) ServiceRequest(args []byte, reply *[]byte) error {
-	fmt.Println("Reached server!!")
-	request := Request{}
+/**
+Method: insert()
+Description: This method inserts the request Triplet into persistent storage if not already present
+*/
+func (dict3 *DICT3) insert(request Request) Response {
+	var key, relation string
+	var value interface{}
+	// Identify the key,relation,value from request object
+	switch interface_type := request.Fields["params"].(type) {
+	case []interface{}:
+		for k, v := range interface_type {
+			if k == 0 {
+				key = v.(string)
+			} else if k == 1 {
+				relation = v.(string)
+			} else if k == 2 {
+				value = v
+			}
+		}
+	}
+	// Update the persistent storage with new triplet
+	inner, ok := dict3.Triplet[key]
+	if !ok {
+		inner = make(map[string]interface{})
+		dict3.Triplet[key] = inner
+	}
+	dict3.Triplet[key][relation] = value
+
+	// Prepare the response object
 	response := Response{}
+	response.Fields = make(map[string]interface{})
+	response.Fields["result"] = true
+	response.Fields["error"] = -1
+	response.Fields["id"] = request.Fields["id"]
+	return response
+}
 
+/**
+Method: lookup()
+Description: This method performs lookup for requested key,relation and returns value if present
+*/
+func (dict3 *DICT3) lookup(request Request) Response {
+	var key, relation string
+	// Identify key,relation from request object
+	switch interface_type := request.Fields["params"].(type) {
+	case []interface{}:
+		for k, v := range interface_type {
+			if k == 0 {
+				key = v.(string)
+			}
+			if k == 1 {
+				relation = v.(string)
+			}
+		}
+	}
+	// Look-up and prepare response object
+	response := Response{}
+	response.Fields = make(map[string]interface{})
+	if val, ok := dict3.Triplet[key][relation]; ok {
+		response.Fields["result"] = val
+		response.Fields["error"] = -1
+	} else {
+		response.Fields["result"] = nil
+		response.Fields["error"] = 1
+	}
+	response.Fields["id"] = request.Fields["id"]
+	return response
+}
+
+func (dict3 *DICT3) shutdown(request Request) {
+	dict3.listener.Close()
+}
+
+/**
+Method: ServiceRequest()
+Description: Accepts requests from the client and calls appropriate method handler
+*/
+func (dict3 *DICT3) ServiceRequest(args []byte, reply *[]byte) error {
+	// Unmarshal data and check for errors
+	request := Request{}
 	err := json.Unmarshal(args, &request.Fields)
 	checkBadRequestError(err)
 
-	switch request.Fields["method"] {
-	case "lookup":
-		response = lookup(request)
-	default:
-		response
-	}
-
+	response := Response{}
 	response.Fields = make(map[string]interface{})
-	response.Fields["response"] = "Successful!"
-	fmt.Println("Marshalled response: ", response)
+
+	// Call appropriate method handler
+	switch request.Fields["method"] {
+	case "insert":
+		response = dict3.insert(request)
+	case "lookup":
+		response = dict3.lookup(request)
+	case "shutdown":
+		dict3.shutdown(request)
+	}
+	// Marshal the response
 	*reply, _ = json.Marshal(&response)
-	fmt.Println("String reply:", string(*reply))
 	return nil
+}
+
+/**
+Method: dumpToPersistentStorage()
+Description: This method dumps the DICT3 data to a file before closing the connection
+*/
+func (dict3 *DICT3) dumpToPersistentStorage(filemap interface{}) {
+	data_bytes, _ := json.Marshal(dict3.Triplet)
+
+	filepath := filemap.(map[string]interface{})["file"].(string)
+	ioutil.WriteFile(filepath, data_bytes, 0644)
+}
+
+/**
+Method: fetchFromPersistentStorage()
+Description: This method retrieves the the DICT3 data from the file
+*/
+func (dict3 *DICT3) fetchFromPersistentStorage(filemap interface{}) {
+	filepath := filemap.(map[string]interface{})["file"].(string)
+	file_content, err := ioutil.ReadFile(filepath)
+	if err == nil {
+		err = json.Unmarshal(file_content, &dict3.Triplet)
+	}
 }
 
 func main() {
 	type_dict3 := new(DICT3)
+	type_dict3.Triplet = make(map[string]map[string]interface{})
 	rpc.Register(type_dict3)
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8010")
+	// Read configuration file
+	var config config_type
+	file, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal("Cannot open config file: ", err)
+	}
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		log.Fatal("Cannot read config file:", err)
+	}
+
+	// Set connection parameters
+	tcpAddr, err := net.ResolveTCPAddr("tcp", config["ipAddress"].(string)+":"+config["port"].(string))
 	checkTCPError(err)
 
 	listener, err := net.ListenTCP("tcp", tcpAddr)
+	type_dict3.listener = listener
 	checkTCPError(err)
-
+	// Retrieve the DICT3 persistent storage
+	type_dict3.fetchFromPersistentStorage(config["persistentStorageContainer"])
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			continue
+			// Close connecton and store DICT3 to a file
+			fmt.Println("About to close connection!")
+			listener.Close()
+			type_dict3.dumpToPersistentStorage(config["persistentStorageContainer"])
+			os.Exit(1)
 		}
 		jsonrpc.ServeConn(conn)
 	}
