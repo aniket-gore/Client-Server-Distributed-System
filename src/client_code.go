68391ec13d922a4cb737db40086bad3339d21fad
aniket-gore <aniketgore05@gmail.com>
2015-03-02 11:03:55 -0500

    Read requests from input file. Sent/Received msgs in a single loop.

--- src/client_code.go
+++ src/client_code.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -46,15 +47,24 @@ func main() {
 	response_channel := make(chan *rpc.Call, 10)
 	var rpc_call *rpc.Call
 
-	// Make a series of asynchronous requests to the server
-	for _, arg := range os.Args[2:] {
+	var count_of_requests int
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		// Make a series of asynchronous requests to the server
+		//	for _, arg := range os.Args[2:] {
 		// Read method name from request JSON object
-		args_bytes := []byte(arg)
+		//		args_bytes := []byte(arg)
+		args_bytes := []byte(scanner.Text())
+		//	fmt.Println("Reqest: ", count_of_requests, " : ", scanner.Text())
+		request = request_type{}
 		err = json.Unmarshal(args_bytes, &request)
+		// Ignore the bad requests and proceed with next requests
 		if err != nil {
-			fmt.Println("Error reading request JSON object.")
+			//	fmt.Println("Error reading request JSON object.")
+			continue
 		}
 
+		count_of_requests += 1
 		// Convert first character of method name to uppercase
 		method_name := request["method"].(string)
 		method_name = string(unicode.ToUpper(rune(method_name[0]))) + method_name[1:]
@@ -62,12 +72,13 @@ func main() {
 		// Make asynchronous request to the server
 		response = nil
 		rpc_call = client.Go(config["serverID"].(string)+"."+method_name, args_bytes, &response, response_channel)
-	}
+		//	}
 
-	// Receive the responses from the server
-	response_msg := response_msg_type{}
-	//	var marshaled_response []byte
-	for i := 0; i < len(os.Args[2:]); i++ {
+		// Receive the responses from the server
+		response_msg := response_msg_type{}
+		//	var marshaled_response []byte
+		//	for i := 0; i < len(os.Args[2:]); i++ {
+		//	for i := 0; i < count_of_requests; i++ {
 		rpc_call = <-response_channel
 		if rpc_call.Error != nil {
 		}
